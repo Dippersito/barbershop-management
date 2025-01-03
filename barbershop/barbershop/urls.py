d15903fd40232b39c66a0f71f84869af3f9671e7
Dippersito <stephano.cornejo@tecsup.edu.pe>
2025-01-03 13:26:14 -0500

    feat: actualización de configuración CORS y settings

--- barbershop/barbershop/urls.py
+++ barbershop/barbershop/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def root_view(request):
+    return HttpResponse("Barbershop Management API v1.0")
 
 urlpatterns = [
+    path('', root_view, name='root'), 
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
 ]
